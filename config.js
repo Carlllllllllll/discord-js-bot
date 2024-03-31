6f3803cc9edda90aade24780413443a26c36362d
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-31 20:24:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  OWNER_IDS: [""], // Bot owner ID's
-  SUPPORT_SERVER: "", // Your bot support server
+  OWNER_IDS: ["1126336222206365696"], // Bot owner ID's
+  SUPPORT_SERVER: "https://discord.gg/HWmdurWCQB", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
     DEFAULT_PREFIX: "!", // Default prefix for the bot
