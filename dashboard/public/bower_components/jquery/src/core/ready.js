19036b7034cd6dabf1c0156832f30ee58d6c6184
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-31 20:52:39 +0200

    Update ready.js

--- dashboard/public/bower_components/jquery/src/core/ready.js
+++ dashboard/public/bower_components/jquery/src/core/ready.js
@@ -1,90 +1,86 @@
-define([
-    "../core",
-    "../var/document",
-    "../core/readyException",
-    "../deferred",
-    "discord.js" // Add Discord.js as a dependency
-], function(jQuery, document, readyException, deferred, Discord) {
+define( [
+	"../core",
+	"../var/document",
+	"../core/readyException",
+	"../deferred"
+], function( jQuery, document ) {
 
-    "use strict";
+"use strict";
 
-    // The deferred used on DOM ready
-    var readyList = jQuery.Deferred();
+// The deferred used on DOM ready
+var readyList = jQuery.Deferred();
 
-    jQuery.fn.ready = function(fn) {
+jQuery.fn.ready = function( fn ) {
 
-        readyList
-            .then(fn)
-            .catch(function(error) {
-                jQuery.readyException(error);
-            });
+	readyList
+		.then( fn )
 
-        return this;
-    };
+		// Wrap jQuery.readyException in a function so that the lookup
+		// happens at the time of error handling instead of callback
+		// registration.
+		.catch( function( error ) {
+			jQuery.readyException( error );
+		} );
 
-    jQuery.extend({
+	return this;
+};
 
-        // Is the DOM ready to be used? Set to true once it occurs.
-        isReady: false,
+jQuery.extend( {
 
-        // A counter to track how many items to wait for before
-        // the ready event fires. See #6781
-        readyWait: 1,
+	// Is the DOM ready to be used? Set to true once it occurs.
+	isReady: false,
 
-        // Handle when the DOM is ready
-        ready: function(wait) {
+	// A counter to track how many items to wait for before
+	// the ready event fires. See #6781
+	readyWait: 1,
 
-            // Abort if there are pending holds or we're already ready
-            if (wait === true ? --jQuery.readyWait : jQuery.isReady) {
-                return;
-            }
+	// Handle when the DOM is ready
+	ready: function( wait ) {
 
-            // Remember that the DOM is ready
-            jQuery.isReady = true;
+		// Abort if there are pending holds or we're already ready
+		if ( wait === true ? --jQuery.readyWait : jQuery.isReady ) {
+			return;
+		}
 
-            // If a normal DOM Ready event fired, decrement, and wait if need be
-            if (wait !== true && --jQuery.readyWait > 0) {
-                return;
-            }
+		// Remember that the DOM is ready
+		jQuery.isReady = true;
 
-            // If there are functions bound, to execute
-            readyList.resolveWith(document, [jQuery]);
-        }
-    });
+		// If a normal DOM Ready event fired, decrement, and wait if need be
+		if ( wait !== true && --jQuery.readyWait > 0 ) {
+			return;
+		}
 
-    jQuery.ready.then = readyList.then;
+		// If there are functions bound, to execute
+		readyList.resolveWith( document, [ jQuery ] );
+	}
+} );
 
-    // The ready event handler and self cleanup method
-    function completed() {
-        document.removeEventListener("DOMContentLoaded", completed);
-        window.removeEventListener("load", completed);
-        jQuery.ready();
-    }
+jQuery.ready.then = readyList.then;
 
-    // Catch cases where $(document).ready() is called
-    // after the browser event has already occurred.
-    // Support: IE <=9 - 10 only
-    // Older IE sometimes signals "interactive" too soon
-    if (document.readyState === "complete" ||
-        (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+// The ready event handler and self cleanup method
+function completed() {
+	document.removeEventListener( "DOMContentLoaded", completed );
+	window.removeEventListener( "load", completed );
+	jQuery.ready();
+}
 
-        // Handle it asynchronously to allow scripts the opportunity to delay ready
-        window.setTimeout(jQuery.ready);
+// Catch cases where $(document).ready() is called
+// after the browser event has already occurred.
+// Support: IE <=9 - 10 only
+// Older IE sometimes signals "interactive" too soon
+if ( document.readyState === "complete" ||
+	( document.readyState !== "loading" && !document.documentElement.doScroll ) ) {
 
-    } else {
+	// Handle it asynchronously to allow scripts the opportunity to delay ready
+	window.setTimeout( jQuery.ready );
 
-        // Use the handy event callback
-        document.addEventListener("DOMContentLoaded", completed);
+} else {
 
-        // A fallback to window.onload, that will always work
-        window.addEventListener("load", completed);
-    }
+	// Use the handy event callback
+	document.addEventListener( "DOMContentLoaded", completed );
 
-    // Discord.js code
-    const { Routes, DataResolver } = Discord;
-    // Assuming `client` is defined and logged in
-    await client.rest.patch(Routes.user(), {
-        body: { banner: await DataResolver.resolveImage("https://i.pinimg.com/originals/0c/0f/2b/0c0f2bec50b55779bb187062e84f7700.gif") }
-    });
+	// A fallback to window.onload, that will always work
+	window.addEventListener( "load", completed );
+}
 
-});
+} );
